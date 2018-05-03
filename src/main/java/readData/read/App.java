32fb0aefdd42baf508b6883aa9840f629ebdcda2
Jorge Martínez <jorge.martinez@2brains.cl>
2018-05-03 14:48:04 -0300

    reading file

--- src/main/java/readData/read/App.java
+++ src/main/java/readData/read/App.java
@@ -2,9 +2,9 @@ package readData.read;
 
 import java.io.File;
 
-import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
 
 /**
  * Hello world!
@@ -14,22 +14,28 @@ public class App
 {
 	
 	public String getImgText(String imageLocation) {
-	      ITesseract instance = new Tesseract();
-	      try 
-	      {
-	         String imgText = instance.doOCR(new File(imageLocation));
-	         return imgText;
-	      } 
-	      catch (TesseractException e) 
-	      {
-	         e.getMessage();
-	         return "Error while reading image";
-	      }
+		File imageFile = new File(imageLocation);
+		Tesseract instance = new Tesseract();
+
+		//In case you don't have your own tessdata, let it also be extracted for you
+		File tessDataFolder = LoadLibs.extractTessResources("tessdata");
+
+		//Set the tessdata path
+		instance.setDatapath(tessDataFolder.getAbsolutePath());
+
+		try {
+		    String result = instance.doOCR(imageFile);
+		    return result;
+		} catch (TesseractException e) {
+			e.getMessage();
+	        return "Error while reading image";
+		}
 	   }
 	
     public static void main( String[] args )
     {
     		App app = new App();
-    		System.out.println(app.getImgText("/Users/jorgemartinez/Desktop/BALANCES/Quinta semana PCGA/10828442/10828442 BALANCE 2015.PDF"));
+    		System.out.println(app.getImgText("C:\\Users\\Jorge\\Downloads\\Quinta semana PCGA\\Quinta semana PCGA\\10828442\\10828442 BALANCE 2015.PDF"));
+    		System.out.println("finished!");
     }
 }
